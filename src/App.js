69b2602e4d4ecc8d185aba6226d01046ecbce1ed
boss <andryousha228@gmail.com>
2022-11-23 01:24:40 +0500

    css correct

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
 import React from 'react'
 import Header from './components/Header'
-import Categories from './components/Categories'
-import SortPopup from './components/SortPopup'
-import PizzaBlock from './components/PizzaBlock'
-import LoadingBlock from './components/LoadingBlock'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import { Routes, Route } from 'react-router-dom';
+
 
 
 
 function App() {
-  
-  const [pizzas,setPizzas]= React.useState([])
-  const [isLoading,setIsLoading]= React.useState(true)
-
-  React.useEffect(()=>{
-  fetch('https://637cafc572f3ce38eaaa7e31.mockapi.io/items').then(
-    (resp)=>resp.json()).then(
-    (arr)=>{setPizzas(arr);
-    setIsLoading(false)})
-
-},[])
 
 
-  
+
+
 
   return (
-    
+
     <div className="wrapper">
       <Header />
-      
+
 
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <SortPopup />
 
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
 
-          {isLoading? [...new Array(4)].map((_,index)=><LoadingBlock key={index}/>) : pizzas.map( (obj)=><PizzaBlock key={obj.id} {...obj}/>)}
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
 
-          
-          </div>
+
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Categories from '../components/Categories'
+import SortPopup from '../components/SortPopup'
+import LoadingBlock from '../components/LoadingBlock'
+import PizzaBlock from '../components/PizzaBlock'
+
+
+
+function Home() {
+  const [pizzas,setPizzas]= React.useState([])
+  const [isLoading,setIsLoading]= React.useState(true)
+
+  React.useEffect(()=>{
+  fetch('https://637cafc572f3ce38eaaa7e31.mockapi.io/items').then(
+    (resp)=>resp.json()).then(
+    (arr)=>{setPizzas(arr)
+    setIsLoading(false)})
+
+},[])
+
+
+  return (
+    <>
+
+      <div className="content__top">
+        <Categories />
+        <SortPopup />
+
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+
+        {isLoading ? [...new Array(4)].map((_, index) => <LoadingBlock key={index} />) : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+
+
+      </div>
+    </>
+
+
+  )
+}
+
+export default Home
+
+
+
+
